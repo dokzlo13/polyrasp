c52d9f5fb4d924b514b63d2e239ab993d99186a1
zloy <dokzlo13@gmail.com>
2018-11-14 19:31:17 +0300

    Error handlers for cache
    Fixes for Cache, always retireve something fom Redis

--- raspisator/app/cache.py
+++ raspisator/app/cache.py
@@ -18,29 +18,43 @@ class Cache:
         self.r = redis
 
     def set_user_week(self, user_id, week_monday):
-        return self.r.set(_week(user_id), week_monday.strftime("%Y-%m-%d %H:%M"), ex=timedelta(minutes=3))
+        return self.r.set(_week(user_id), week_monday.strftime("%Y-%m-%d %H:%M"))
 
     def get_user_week(self, user_id):
         try:
-            w = datetime.strptime(self.r.get(_week(user_id)).decode('utf-8'), "%Y-%m-%d %H:%M")
+            msg = self.r.get(_week(user_id))
+            if not msg:
+                return None
+            w = datetime.strptime(msg.decode('utf-8'), "%Y-%m-%d %H:%M")
         except TypeError:
             return None
         else:
             return w
 
     def set_user_cal(self, user_id, cal):
-        return self.r.set(_cal(user_id), json.dumps(cal), ex=timedelta(minutes=3))
+        return self.r.set(_cal(user_id), json.dumps(cal))
 
     def get_user_cal(self, user_id):
         try:
-         c = json.loads(self.r.get(_cal(user_id)))
+            msg = self.r.get(_cal(user_id))
+            if not msg:
+                return None
+            c = json.loads(msg)
         except TypeError:
             return None
         else:
             return c
 
     def set_user_curr_gr(self, user_id, group):
-        return self.r.set(_gr(user_id), group, ex=timedelta(minutes=3))
+        return self.r.set(_gr(user_id), group)
 
     def get_user_curr_gr(self, user_id):
-        return self.r.get(_gr(user_id))
+        try:
+            msg = self.r.get(_gr(user_id))
+            if not msg:
+                return None
+            g = msg.decode('utf-8')
+        except TypeError:
+            return None
+        else:
+            return g
