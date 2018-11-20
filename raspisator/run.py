02a412fe760e77176a7d02f95204335200c10a9f
zloy <dokzlo13@gmail.com>
2018-11-20 16:21:38 +0300

    Close dialog for inline changings

--- raspisator/run.py
+++ raspisator/run.py
@@ -3,17 +3,26 @@
 
 # bot.polling(timeout=10)
 import time
+import sys, traceback
 
-from telebot import logger
-
+from telebot import logger, apihelper
 from app import bot
+
+# bot.polling(timeout=40, interval=0, none_stop=False)
+
+apihelper.CONNECT_TIMEOUT = 15
+# updates = bot.get_updates(offset=(bot.last_update_id + 1), timeout=timeout)
+# bot.process_new_updates(updates)
+
 
 while True:
     try:
-        bot.polling(timeout=40, interval=0, none_stop=False)
+        bot.polling(timeout=30, interval=1, none_stop=False)
     except KeyboardInterrupt:
         bot.stop_bot()
         exit(0)
     except Exception as e:
         logger.error("Error polling info: \"{0}\"".format(e))
+        exc_info = sys.exc_info()
+        traceback.print_exception(*exc_info)
         time.sleep(1)
